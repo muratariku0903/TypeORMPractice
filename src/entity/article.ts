e7cad9c7a75fceda8959c36e11673e1ccbf72b19
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-06-21 23:30:40 +0900

    wip

---
+++ src/entity/article.ts
@@ -0,0 +1,26 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Article {
+    @PrimaryGeneratedColumn()
+    id!: string
+
+    @Column({ unique: true })
+    url!: string
+
+    @Column()
+    title!: string
+
+    @Column('text')
+    content!: string
+
+    @Column('varchar', { nullable: true })
+    note!: string | null
+
+    @Column({ default: 0 })
+    start!: number
+
+    @Column()
+    created!: Date
+}
