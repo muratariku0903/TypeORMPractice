ad80cb0587684a567a1ad8efe45cbfbc7537b3de
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-03 20:19:20 +0900

    initialize db

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@ import { DataSource } from "typeorm";
 const AppDataSource = new DataSource({
   type: 'postgres',
   host: "localhost",
-  port: 3306,
+  port: 5432,
   username: "test",
-  password: "test",
+  password: "passw0rd",
   database: "test",
 });
 
-AppDataSource.initialize().then();
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err)
+  })
