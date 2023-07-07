aeec0ac85f9c3551bdcb656a962531ef55ff2752
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-08 01:16:51 +0900

    add eslint and prettier

--- src/common/db.ts
+++ src/common/db.ts
@@ -1,29 +1,37 @@
-import { DataSource } from "typeorm";
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+import { DataSource } from 'typeorm'
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions'
 
 const dbConfig: PostgresConnectionOptions = {
   type: 'postgres',
-  host: "localhost",
+  host: 'localhost',
   port: 5432,
-  username: "test",
-  password: "passw0rd",
-  database: "test",
+  username: 'test',
+  password: 'passw0rd',
+  database: 'test',
+  schema: 'public',
 }
 
-const AppDataSource = new DataSource(dbConfig);
+const AppDataSource = new DataSource(dbConfig)
 
-const initDataSource = async () => {
-  console.info();
+export const initDataSource = async () => {
+  console.info(`${initDataSource.name} start`)
 
   try {
     if (!AppDataSource.isInitialized) {
-      await AppDataSource.initialize();
+      await AppDataSource.initialize()
     }
 
     if (!AppDataSource.isInitialized) {
-      console.log('error ')
+      throw new Error('DataSource initialize failed.')
     }
   } catch (e) {
-
+    console.error(e)
+  } finally {
+    console.info(`${initDataSource.name} end`)
+    console.log('hello')
   }
+}
+
+export const closeDataSource = async () => {
+  // conso
 }
