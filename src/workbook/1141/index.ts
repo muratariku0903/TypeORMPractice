40da1bf4ba8e3139c1814e98705a64ea0aa11ebe
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-12 19:34:30 +0900

    wip

--- src/workbook/1141/index.ts
+++ src/workbook/1141/index.ts
@@ -5,8 +5,24 @@ const solve = async () => {
   try {
     await initDataSource()
 
-    const res = await AppDataSource.createQueryBuilder(Activity1141, 'activity').getMany()
-    // const res = await AppDataSource.createQueryBuilder(View, 'views').getRawMany<View>()
+    // 発行したいクエリ↓
+    // select activity_date as day, count(distinct user_id) as active_users from Activity group by activity_date having min(activity_date) >= "2019-06-28" AND max(activity_date) <= "2019-07-27";
+
+    // const queryBuilder = this.createQueryBuilder('activity')
+    //   .select('activity.activity_date', 'day')
+    //   .addSelect('COUNT(DISTINCT activity.user_id)', 'active_users')
+    //   .where('activity.activity_date >= :startDate', { startDate })
+    //   .andWhere('activity.activity_date <= :endDate', { endDate })
+    //   .groupBy('activity.activity_date');
+
+    // return queryBuilder.getRawMany();
+
+    const res = await AppDataSource
+      .createQueryBuilder(Activity1141, 'activity')
+      .select('activity.activity_date as day count(distinct activity.user_id) as active_users')
+      // .select('activity.activity_date as day count(distinct activity.user_id) as active_users')
+      .groupBy('activity.activity_date')
+      .getRawMany();
 
     console.log(res)
   } catch (e) {
---
+++ src/workbook/1141/sql/alter.sql
@@ -0,0 +1,3 @@
+# 既存のテーブルにPKを追加
+ALTER TABLE public.activity1141
+ADD COLUMN id SERIAL PRIMARY KEY;
