5b4b3dc0fa9a6f76715446e36966c1e2abd4c5a5
muratariku0903 <18ba179r@rikkyo.ac.jp>
2023-07-28 19:36:51 +0900

    wip

--- src/workbook/1795/index.ts
+++ src/workbook/1795/index.ts
@@ -19,36 +19,54 @@ const solve = async () => {
     // WHERE store3 IS NOT NULL;
 
     const queries = [
-      `(${AppDataSource.createQueryBuilder(Products1795, 'p')
-        // .select('p.product_id AS product_id')
+      AppDataSource.createQueryBuilder(Products1795, 'p')
         .select('p.product_id', 'product_id')
         .addSelect('store1', 'store')
+        // .addSelect('"store1" as store')
         .addSelect('p.store1', 'price')
         .where('p.store1 IS NOT NULL')
-        // .getQuery()})`,
-        .getQuery()}) AS Q1`,
-      `(${AppDataSource.createQueryBuilder(Products1795, 'p')
+        .getQuery(),
+      AppDataSource.createQueryBuilder(Products1795, 'p')
         .select('p.product_id', 'product_id')
         .addSelect('store2', 'store')
         .addSelect('p.store2', 'price')
         .where('p.store2 IS NOT NULL')
-        // .getQuery()})`,
-        .getQuery()}) AS Q2`,
-      `(${AppDataSource.createQueryBuilder(Products1795, 'p')
+        .getQuery(),
+      AppDataSource.createQueryBuilder(Products1795, 'p')
         .select('p.product_id', 'product_id')
         .addSelect('store3', 'store')
         .addSelect('p.store3', 'price')
         .where('p.store3 IS NOT NULL')
-        // .getQuery()})`,
-        .getQuery()}) AS Q3`,
+        .getQuery(),
     ]
 
     const unionQuery = queries.join(' UNION ')
+
     console.log(unionQuery)
 
-    const res = await AppDataSource.createQueryBuilder()
+    // const query = AppDataSource.createQueryBuilder().select('product_id,store,price').from(unionQuery, '')
+    // console.log(query.getQuery())
+
+    const query = await AppDataSource.query(unionQuery)
+
+    console.log(query)
+
+    // const q = AppDataSource.createQueryBuilder().select('uq.product_id, uq.store, uq.price').from(unionQuery, 'uq')
+    // console.log(q.getQuery())
+    // console.log(t)
+
+    return
+
+    // return;
+
+    const res = await AppDataSource.createQueryBuilder(Products1795, 'p')
       .select('product_id, store, price')
-      .from(unionQuery, 'uq')
+      .from(
+        'SELECT p.product_id AS "product_id", p.store1 AS price, store1 AS store FROM "public"."products1795" "p" WHERE "p"."store1" IS NOT NULL',
+        ''
+      )
+      // .from(unionQuery, 'uq')
+      // .from(, 'uq')
       // .addFrom(`(${store2Query})`, 'q2')
       // .addFrom(`(${store3Query})`, 'q3')
       // // .where('1=0')
@@ -57,7 +75,7 @@ const solve = async () => {
       // .orWhere('product_id = q3.product_id')
       .getRawMany()
 
-    console.log(res)
+    // console.log(res)
   } catch (e) {
     console.log(e)
   } finally {
